c71c13e9dafe96b43882e8a74ceafb0667bc22e6
Is_human <goofyjnr@gmail.com>
2025-04-20 15:47:10 +1200

    added the index and did some basic layout stuff for the first assignment

---
+++ htdocs/assign1/poststatusform.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="COMP721 Assignment 1">
+        <meta name="keywords" content="Web Development, Assignment 1">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>About</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+
+            <div class="nav_bar">
+                <a href="index.html" class="btn">Home</a>
+                <a href="poststatusform.php" class="btn">Post a new status</a>
+                <a href="searchstatusform.html" class="btn">Search status</a>
+                <a href="about.html" class="btn">About the assignment</a>
+
+            </div>
+
+            <!--Post status code-->
+
+
+
+        </div>
+    </body>
+
+    <footer>
+        <p>Alister Faid 22171016</p>
+    </footer>
+</html>
