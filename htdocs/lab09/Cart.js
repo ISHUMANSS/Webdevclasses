cadcf3dfceeec3693d4482005a72bd5ebe6123bc
Is_human <goofyjnr@gmail.com>
2025-05-21 17:32:03 +1200

    finished lab 9

--- htdocs/lab09/Cart.js
+++ htdocs/lab09/Cart.js
@@ -1,60 +1,210 @@
 var xHRObject = false;
-if (window.XMLHttpRequest)
-{
-xHRObject = new XMLHttpRequest();
-}
-else if (window.ActiveXObject)
-{
-xHRObject = new ActiveXObject("Microsoft.XMLHTTP");
+if (window.XMLHttpRequest) {
+    xHRObject = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    xHRObject = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-
-function getData()
-{
-    if ((xHRObject.readyState == 4) &&(xHRObject.status == 200))
-    {
-		var serverResponse = xHRObject.responseXML;
-		alert(serverResponse);
+function getData() {
+    if ((xHRObject.readyState == 4) && (xHRObject.status == 200)) {
+        var serverResponse = xHRObject.responseXML;
         var spantag = document.getElementById("cart");
-
-        if (serverResponse != null){
-            var header = serverResponse.getElementsByTagName("book");
-            spantag.innerHTML = "";
-            for (i=0; i<header.length; i++)
-            {
-           
-            if (window.ActiveXObject)
-            {
-                spantag.innerHTML += " " +header[0].firstChild.text;
-                spantag.innerHTML += " " + header[0].lastChild.text + " " + "<a href='#' onclick='AddRemoveItem(\"Remove\");'>Remove Item</a>";
+        
+        //clear cart display
+        spantag.innerHTML = "";
+        
+        if (serverResponse != null) {
+            var books = serverResponse.getElementsByTagName("book");
+            
+            //create table for the cart
+            var cartTable = document.createElement("table");
+            cartTable.setAttribute("border", "1");
+            cartTable.style.borderCollapse = "collapse";
+            
+            //table header
+            var headerRow = document.createElement("tr");
+            var headers = ["Title", "ISBN", "Price", "Quantity", "Subtotal", "Action"];
+            
+            headers.forEach(function(headerText) {
+                var th = document.createElement("th");
+                th.textContent = headerText;
+                th.style.padding = "5px";
+                headerRow.appendChild(th);
+            });
+            
+            cartTable.appendChild(headerRow);
+            
+            //add each book to the table
+            for (var i = 0; i < books.length; i++) {
+                var book = books[i];
+                var row = document.createElement("tr");
+                
+                //get book details
+                var title, isbn, price, quantity;
+                
+                if (window.ActiveXObject) {
+                    title = book.getElementsByTagName("title")[0].text;
+                    isbn = book.getElementsByTagName("isbn")[0].text;
+                    price = book.getElementsByTagName("price")[0].text;
+                    quantity = book.getElementsByTagName("quantity")[0].text;
+                } else {
+                    title = book.getElementsByTagName("title")[0].textContent;
+                    isbn = book.getElementsByTagName("isbn")[0].textContent;
+                    price = book.getElementsByTagName("price")[0].textContent;
+                    quantity = book.getElementsByTagName("quantity")[0].textContent;
+                }
+                
+                //calculate subtotal
+                var subtotal = parseFloat(price) * parseInt(quantity);
+                
+                //create cells
+                var cells = [title, isbn, "$" + price, quantity, "$" + subtotal.toFixed(2)];
+                
+                cells.forEach(function(cellText) {
+                    var td = document.createElement("td");
+                    td.textContent = cellText;
+                    td.style.padding = "5px";
+                    row.appendChild(td);
+                });
+                
+                //add remove button
+                var actionCell = document.createElement("td");
+                var removeLink = document.createElement("a");
+                removeLink.href = "#";
+                removeLink.textContent = "Remove";
+                removeLink.style.padding = "5px";
+                removeLink.onclick = function() {
+                    AddRemoveItem("Remove", title, isbn, price);
+                    return false;
+                };
+                actionCell.appendChild(removeLink);
+                row.appendChild(actionCell);
+                
+                cartTable.appendChild(row);
             }
-            else
-            {
-                spantag.innerHTML += " " +header[0].firstChild.textContent;
-                spantag.innerHTML += " " + header[0].lastChild.textContent + " " + "<a href='#' onclick='AddRemoveItem(\"Remove\");'>Remove Item</a>";
+            
+            //add total cost row
+            var totalCost;
+            if (window.ActiveXObject) {
+                totalCost = serverResponse.getElementsByTagName("totalcost")[0].text;
+            } else {
+                totalCost = serverResponse.getElementsByTagName("totalcost")[0].textContent;
             }
-            }
+            
+            var totalRow = document.createElement("tr");
+            var totalLabelCell = document.createElement("td");
+            totalLabelCell.setAttribute("colspan", "4");
+            totalLabelCell.textContent = "Total Cost:";
+            totalLabelCell.style.textAlign = "right";
+            totalLabelCell.style.fontWeight = "bold";
+            totalRow.appendChild(totalLabelCell);
+            
+            var totalValueCell = document.createElement("td");
+            totalValueCell.setAttribute("colspan", "2");
+            totalValueCell.textContent = "$" + totalCost;
+            totalValueCell.style.fontWeight = "bold";
+            totalRow.appendChild(totalValueCell);
+            
+            cartTable.appendChild(totalRow);
+            
+            //add the table to the cart span
+            spantag.appendChild(cartTable);
         }
-        else{  spantag.innerHTML = ""; }
     }
 }
 
-function AddRemoveItem(action)
-{
-          var book  = document.getElementById("book").innerHTML;
-           
-          if(action=="Add")
-          {
-            xHRObject.open("GET", "ManageCart.php?action=" + action + "&book=" + encodeURIComponent(book) + "&value=" + Number(new Date), true);
-          }
-          else
-          {
-           xHRObject.open("GET", "ManageCart.php?action=" + action + "&book=" + encodeURIComponent(book) + "&value="  + Number(new Date), true);
-          }
-
-          xHRObject.onreadystatechange = getData;
-          xHRObject.send(null);   
+function AddRemoveItem(action, bookTitle, bookISBN, bookPrice) {
+    //if parameters not provided get them from the current book display
+    if (!bookTitle) {
+        bookTitle = document.getElementById("book").innerText;
+    }
+    if (!bookISBN) {
+        bookISBN = document.getElementById("ISBN").innerText;
+    }
+    if (!bookPrice) {
+        bookPrice = document.getElementById("price").innerText.replace('$', '');
+    }
+    
+    var url = "ManageCart.php?action=" + action + 
+              "&book=" + encodeURIComponent(bookTitle) + 
+              "&isbn=" + encodeURIComponent(bookISBN) + 
+              "&price=" + encodeURIComponent(bookPrice) + 
+              "&value=" + Number(new Date);
+    
+    xHRObject.open("GET", url, true);
+    xHRObject.onreadystatechange = getData;
+    xHRObject.send(null);   
 }
 
+//load multiple books when page loads
+function loadCatalog() {
+    var xhrCatalog = new XMLHttpRequest();
+    xhrCatalog.open("GET", "json/catalog.json", true);
+    xhrCatalog.onreadystatechange = function() {
+        if (xhrCatalog.readyState == 4 && xhrCatalog.status == 200) {
+            var catalog = JSON.parse(xhrCatalog.responseText);
+            displayCatalog(catalog);
+        }
+    };
+    xhrCatalog.send(null);
+}
 
-
+function displayCatalog(catalog) {
+    var catalogDiv = document.getElementById("catalog");
+    catalogDiv.innerHTML = "";
+    
+    catalog.books.forEach(function(book) {
+        var bookDiv = document.createElement("div");
+        bookDiv.className = "book-item";
+        bookDiv.style.border = "1px solid #ccc";
+        bookDiv.style.padding = "10px";
+        bookDiv.style.margin = "10px 0";
+        
+        //book cover
+        var coverImg = document.createElement("img");
+        coverImg.src = book.coverImage || "default_cover.jpg";
+        coverImg.style.width = "100px";
+        coverImg.style.display = "block";
+        coverImg.style.marginBottom = "10px";
+        bookDiv.appendChild(coverImg);
+        
+        //book details
+        var detailsDiv = document.createElement("div");
+        
+        //title
+        var titleSpan = document.createElement("div");
+        titleSpan.innerHTML = "<b>Book:</b> <span class='book-title'>" + book.title + "</span>";
+        detailsDiv.appendChild(titleSpan);
+        
+        //authors
+        var authorsSpan = document.createElement("div");
+        authorsSpan.innerHTML = "<b>Authors:</b> <span class='book-authors'>" + book.authors + "</span>";
+        detailsDiv.appendChild(authorsSpan);
+        
+        //ISBN
+        var isbnSpan = document.createElement("div");
+        isbnSpan.innerHTML = "<b>ISBN:</b> <span class='book-isbn'>" + book.isbn + "</span>";
+        detailsDiv.appendChild(isbnSpan);
+        
+        //price
+        var priceSpan = document.createElement("div");
+        priceSpan.innerHTML = "<b>Price:</b> <span class='book-price'>$" + book.price + "</span>";
+        detailsDiv.appendChild(priceSpan);
+        
+        bookDiv.appendChild(detailsDiv);
+        
+        //add to cart button
+        var addButton = document.createElement("a");
+        addButton.href = "#";
+        addButton.textContent = "Add to Shopping Cart";
+        addButton.style.display = "inline-block";
+        addButton.style.marginTop = "10px";
+        addButton.onclick = function() {
+            AddRemoveItem("Add", book.title, book.isbn, book.price);
+            return false;
+        };
+        bookDiv.appendChild(addButton);
+        
+        catalogDiv.appendChild(bookDiv);
+    });
+}
--- htdocs/lab09/ManageCart.php
+++ htdocs/lab09/ManageCart.php
@@ -1,64 +1,101 @@
-<?php
+<?php 
 session_start();
 header('Content-Type: text/xml');
 ?>
 <?php
-        $newitem = $_GET["book"];
+
+        $bookTitle = $_GET["book"];
+        $isbn = $_GET["isbn"];
+        $price = $_GET["price"];
         $action = $_GET["action"];
-        //if ($_SESSION["Cart"] != "")
-		if (array_key_exists("Cart", $_SESSION))
-        {
-            $cart1 = $_SESSION["Cart"];
-            if ($action == "Add")
-            {
-                if ($cart1[$newitem] != "")
-                {  
-                    $value = $cart1[$newitem] + 1;
-                    $cart1[$newitem] = $value;
-                }
-                else
-                {
-                    $cart1[$newitem] = "1";
+
+        //create the cart if it doesn't exist
+        if (!array_key_exists("Cart", $_SESSION)) {
+            $_SESSION["Cart"] = array();
+        }
+
+        $cart1 = $_SESSION["Cart"];
+
+
+        if ($action == "Add") {
+            if (array_key_exists($bookTitle, $cart1)) {
+                //add more of an existing book
+                $cart1[$bookTitle]["quantity"] += 1;
+            } else {
+                //add a new book to the cart
+                $cart1[$bookTitle] = array(
+                    "isbn" => $isbn,
+                    "price" => $price,
+                    "quantity" => 1
+                );
+            }
+        } else if ($action == "Remove") {
+            if (array_key_exists($bookTitle, $cart1)) {
+                //take one of the specific book out of the cart
+                $cart1[$bookTitle]["quantity"] -= 1;
+                
+                //remove the item if there are none left
+                if ($cart1[$bookTitle]["quantity"] <= 0) {
+                    unset($cart1[$bookTitle]);
                 }
             }
-            else
-            {
-                $cart1[$newitem]= "";
+        }
+
+        //re apply the cart to the session
+        $_SESSION["Cart"] = $cart1;
+
+
+        //only return XML if cart has items
+        if (count($cart1) > 0) {
+            echo toXml($cart1);
+        }
+
+
+
+        function toXml($cart1) {
+            $doc = new DomDocument('1.0');
+            $cart = $doc->createElement('cart');
+            $cart = $doc->appendChild($cart);
+            
+            //calculate the total cost
+            $totalCost = 0;
+            
+            //add all the books to the xml
+            foreach ($cart1 as $title => $details) {
+                $book = $doc->createElement('book');
+                $book = $cart->appendChild($book);
+                
+                $titleElem = $doc->createElement('title'); 
+                $titleElem = $book->appendChild($titleElem);   
+                $value = $doc->createTextNode($title);
+                $value = $titleElem->appendChild($value);
+                
+                $isbnElem = $doc->createElement('isbn');
+                $isbnElem = $book->appendChild($isbnElem);
+                $isbnValue = $doc->createTextNode($details["isbn"]);
+                $isbnValue = $isbnElem->appendChild($isbnValue);
+                
+                $priceElem = $doc->createElement('price');
+                $priceElem = $book->appendChild($priceElem);
+                $priceValue = $doc->createTextNode($details["price"]);
+                $priceValue = $priceElem->appendChild($priceValue);
+                
+                $quantity = $doc->createElement('quantity');
+                $quantity = $book->appendChild($quantity);
+                $value2 = $doc->createTextNode($details["quantity"]);
+                $value2 = $quantity->appendChild($value2);
+                
+                //add to total cost
+                $totalCost += $details["price"] * $details["quantity"];
             }
-        }
-        else
-        {
-            $cart1[$newitem] = "1";
-        }
-        $_SESSION["Cart"] = $cart1; 
-        if ($cart1[$newitem] != "") 
-			echo (toXml($cart1));         								
-    
-    function toXml($cart1)
-    {
-        $doc = new DomDocument('1.0');
-        $cart = $doc->createElement('cart');
-        $cart = $doc->appendChild($cart);
-        
-        foreach ($cart1 as $a => $b)
-        {
-        
-        $book = $doc->createElement('book');
-        $book = $cart->appendChild($book);
-
-        $title = $doc->createElement('title'); 
-        $title = $book->appendChild($title);   
-        $value = $doc->createTextNode($a);
-        $value = $title->appendChild($value);
-
-        $quantity = $doc->createElement('quantity');
-        $quantity = $book->appendChild($quantity);
-        $value2 = $doc->createTextNode($b);
-        $value2 = $quantity->appendChild($value2);
-     
-      }
-
-        $strXml = $doc->saveXML(); 
-        return $strXml;
+            
+            //add total cost to XML
+            $totalElem = $doc->createElement('totalcost');
+            $totalElem = $cart->appendChild($totalElem);
+            $totalValue = $doc->createTextNode(number_format($totalCost, 2));
+            $totalValue = $totalElem->appendChild($totalValue);
+            
+            $strXml = $doc->saveXML(); 
+            return $strXml;
     }
 ?>
