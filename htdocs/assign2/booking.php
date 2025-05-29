34af924e2c3340244b562072f33037c2e4be1a01
Is_human <goofyjnr@gmail.com>
2025-05-29 13:38:16 +1200

    comment fixes

--- htdocs/assign2/booking.php
+++ htdocs/assign2/booking.php
@@ -67,8 +67,8 @@
         $pickupDateTime = date("Y-m-d H:i:s", strtotime("$date $time"));
 
         //create the booking refrence
-        //uses "BRN00001" format
-        $result = mysqli_query($conn, "SELECT MAX(id) as max_id FROM bookings");
+        //uses BRN00001 format
+        $result = mysqli_query($conn, "SELECT MAX(id) as max_id FROM bookings");//get the highest id
         $row = mysqli_fetch_assoc($result);
         $nextId = $row['max_id'] + 1;
         $bookingRef = "BRN" . str_pad($nextId, 5, "0", STR_PAD_LEFT);
@@ -78,17 +78,20 @@
         $stmt = $conn->prepare("INSERT INTO bookings (
             bookingref, cname, phone, unumber, snumber, stname, sbname, dsbname, date, time
         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        //add all the user details to the insert
         $stmt->bind_param(
             "ssssssssss",
             $bookingRef, $cname, $phone, $unumber, $snumber, $stname, $sbname, $dsbname, $date, $time
         );
 
-        //execute the statement
+        //run the statement
         if ($stmt->execute()) {
             //format confirmation
             $pickupTimeFormatted = date("H:i", strtotime($time));
             $pickupDateFormatted = date("d/m/Y", strtotime($date));
 
+            //give the response to the js
             echo "Thank you for your booking!<br>
                 Booking reference number: $bookingRef<br>
                 Pickup time: $pickupTimeFormatted<br>
