05a906b901560bc819414c128d3abdd591f4b5df
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-06-09 18:20:17 +1200

    Update settings.php

--- files/settings.php
+++ files/settings.php
@@ -1,9 +1,9 @@
 <?php
     
-    $host="webdev.aut.ac.nz";
-	$user="mtr9138";
-	$pswd="ufcuvkeinnzrfsmyxbghfcourqekrzymx";
-	$dbnm="mtr9138";
+    $host="domain";
+	$user="username";
+	$pswd="password";
+	$dbnm="name";
 
     //set your own table in the file like in the sqlinfo.inc.php file
 
