80d28216150b71c6ade877bea8636b109abd42e5
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-06-09 18:20:50 +1200

    Update sqlinfo.inc.php

--- files/sqlinfo.inc.php
+++ files/sqlinfo.inc.php
@@ -4,10 +4,10 @@
 // please change to your user, password, and db
 // please put it under /home/<user>/conf/
 
-	$sql_host="webdev.aut.ac.nz";
-	$sql_user="mtr9138";
-	$sql_pass="ufcuvkeinnzrfsmyxbghfcourqekrzymx";
-	$sql_db="mtr9138";
-	$sql_tble="cars";
+	$sql_host="domain";
+	$sql_user="name";
+	$sql_pass="pass";
+	$sql_db="name";
+	$sql_tble="table";
 ?>
 
